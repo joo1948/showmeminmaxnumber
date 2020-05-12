d5c58aba6ff8cb4681297aad9cb52a7f02e00481
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2020-05-12 12:49:26 +0900

    문제7번 showmeminmaxnumber

--- showmeminmaxnumber/Program.cs
+++ showmeminmaxnumber/Program.cs
@@ -10,6 +10,24 @@ namespace showmeminmaxnumber
     {
         static void Main(string[] args)
         {
+            int[] num = new int[5];
+            for(int i = 0; i < 5; i++)//입력하는 for문
+            {
+                Console.Write("숫자를 입력해주세요: ");
+                num[i]=int.Parse(Console.ReadLine());
+            }
+            int min = num[0], max = num[0];
+            for(int i = 0; i < 5; i++)//min, max 구하는 for문
+            {
+                for(int j = 0; j <= i; j++)
+                {
+                    if (min > num[j]) min = num[j];
+                    if (max < num[j]) max = num[j];
+                }
+            }
+            Console.WriteLine("------------------------");
+            Console.WriteLine("가장 큰 수 : " + max);
+            Console.WriteLine("가장 작은 수 : " + min);
         }
     }
 }
